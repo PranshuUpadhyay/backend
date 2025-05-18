0882dae9ecd426bdd576a708c4b81a9f297f3820
Pranshu <pranshuupadhayay@gmail.com>
2025-05-18 13:30:43 +0530

    SSL

--- config/db.js
+++ config/db.js
@@ -1,3 +1,11 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_URL || 'postgres://postgres:postgres@localhost:5432/journal_toddle');
+const sequelize = new Sequelize(process.env.DB_URL || 'postgres://postgres:postgres@localhost:5432/journal_toddle' , {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // Enforce SSL
+      rejectUnauthorized: false, // Allow self-signed certificates
+    },
+  },
+});
 module.exports = sequelize;
